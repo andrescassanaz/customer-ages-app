0f5263c5ac13f39f05ba5e03c6364bef325a4c5c
Andres Cassanaz <andres.cassanaz@redb.ee>
2021-04-29 19:49:03 -0300

    seed springboot

---
+++ src/main/java/com/andrescassanaz/customeragesapp/CustomerAgesAppApplication.java
@@ -0,0 +1,13 @@
+package com.andrescassanaz.customeragesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAgesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAgesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrescassanaz/customeragesapp/CustomerAgesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrescassanaz.customeragesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAgesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
